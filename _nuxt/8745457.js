4ada30f10e0869acee552c828a136b7043a8517c
John Michael Asejo <jmasejo@gmail.com>
2020-10-15 15:41:03 +0800

    fixed GA

---
+++ _nuxt/8745457.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{180:function(n,t,e){"use strict";var c=e(59),component=Object(c.a)({},(function(){var n=this.$createElement,t=this._self._c||n;return t("div",[t("Nuxt")],1)}),[],!1,null,null,null);t.a=component.exports},181:function(n,t,e){e(182),n.exports=e(183)}},[[181,3,1,4]]]);
